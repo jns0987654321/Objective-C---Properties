6facb677b42f0e5f8b822ce7187a673d85d1cbb8
Junior Samaroo <jns0987654321@gmail.com>
2017-04-23 14:56:38 -0400

    Initial Commit

---
+++ Properties/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Properties
+//
+//  Created by Junior Samaroo on 2017-04-23.
+//  Copyright © 2017 Junior Samaroo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
